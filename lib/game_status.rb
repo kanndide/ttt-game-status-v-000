608a9c9a3bc1d57d25cb762088584cb1963ba93b
William Kann <william.l.kann@gmail.com>
2017-12-03 18:34:14 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,6 +28,8 @@ def won?(board)
       return win_combination # return the win_combination indexes that won.
     end
   end
+  win_combination.is_a?(array) != true
+  false
 end
 
 def full?(board)
