7556820ab53e41a94418a448a3d75d27f1eaf5b2
William Kann <william.l.kann@gmail.com>
2017-12-03 19:15:19 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -47,7 +47,8 @@ end
 
 def winner(board)
   won?(board)
-  puts "#{board(won?(board))}"
+  winner = won?board
+  puts "#{board[won?(board)]}"
 end
 
 board = ["X", "X", "X", "O", "O", " ", " ", " ", " "]
